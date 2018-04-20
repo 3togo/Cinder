3togo/Cinder

commit 5e14fdc184bdf95f812fca4352b74a654cb3b5d4

added a timer to the Context prep and post process loop

include/cinder/audio/Context.h
@@ -26,6 +26,7 @@
 #include "cinder/audio/Node.h"
 #include "cinder/audio/InputNode.h"
 #include "cinder/audio/OutputNode.h"
+#include "cinder/Timer.h"
 
 #include <list>
 #include <mutex>
@@ -129,6 +130,8 @@ class CI_API Context : public std::enable_shared_from_this<Context> {
 	void preProcess();
 	//! OutputNode implementations should call this after each rendering block.
 	void postProcess();
+	//! Returns the time in seconds spent during the last process loop.
+	double getTimeDuringLastProcessLoop() const	{ return mTimeDuringLastProcessLoop; }
 
 	//! Returns a string representation of the Node graph for debugging purposes.
 	std::string printGraphToString();
@@ -164,8 +167,10 @@ class CI_API Context : public std::enable_shared_from_this<Context> {
 	std::atomic<uint64_t>		mNumProcessedFrames;
 	OutputNodeRef				mOutput;
 	std::list<ScheduledEvent>	mScheduledEvents;
+	ci::Timer					mProcessTimer;
+	std::atomic<double>			mTimeDuringLastProcessLoop = -1;
 
-	// other nodes that don't have any outputs and need to be explictly pulled
+	// other nodes that don't have any outputs and need to be explicitly pulled
 	std::set<NodeRef>		mAutoPulledNodes;
 	std::vector<Node *>		mAutoPullCache;
 	bool					mAutoPullRequired, mAutoPullCacheDirty;

src/cinder/audio/Context.cpp
@@ -296,6 +296,7 @@ bool Context::isAudioThread() const
 
 void Context::preProcess()
 {
+	mProcessTimer.start();
 	mAudioThreadId = std::this_thread::get_id();
 
 	preProcessScheduledEvents();
@@ -306,6 +307,9 @@ void Context::postProcess()
 	processAutoPulledNodes();
 	postProcessScheduledEvents();
 	incrementFrameCount();
+
+	mProcessTimer.stop();
+	mTimeDuringLastProcessLoop = mProcessTimer.getSeconds();
 }
 
 void Context::incrementFrameCount()

commit 352653fc23a65f5d3b1f66a25ed3c8d3d9886509

Fix compile error with clang
Caused in 5e14fdc184bdf95f812fca4352b74a654cb3b5d4

include/cinder/audio/Context.h
@@ -168,7 +168,7 @@ class CI_API Context : public std::enable_shared_from_this<Context> {
 	OutputNodeRef				mOutput;
 	std::list<ScheduledEvent>	mScheduledEvents;
 	ci::Timer					mProcessTimer;
-	std::atomic<double>			mTimeDuringLastProcessLoop = -1;
+	std::atomic<double>			mTimeDuringLastProcessLoop;
 
 	// other nodes that don't have any outputs and need to be explicitly pulled
 	std::set<NodeRef>		mAutoPulledNodes;

src/cinder/audio/Context.cpp
@@ -137,7 +137,7 @@ void Context::setMaster( Context *masterContext, DeviceManager *deviceManager )
 }
 
 Context::Context()
-	: mEnabled( false ), mAutoPullRequired( false ), mAutoPullCacheDirty( false ), mNumProcessedFrames( 0 )
+	: mEnabled( false ), mAutoPullRequired( false ), mAutoPullCacheDirty( false ), mNumProcessedFrames( 0 ), mTimeDuringLastProcessLoop( -1.0 )
 {
 }
 